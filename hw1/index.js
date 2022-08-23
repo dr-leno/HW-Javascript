3a05eb953025e98d144d71f853730d6ad7a80e62
dr-leno <87282003+dr-leno@users.noreply.github.com>
2022-08-24 01:34:04 +0300

    update index js

--- hw1/index.js
+++ hw1/index.js
@@ -3,7 +3,7 @@
 const firstNumber = Number(prompt('Enter first number', 2));
 const secondNumber = Number(prompt('Enter second number', 3));
 
-// If user will choose cancel - it means false. False to Number = 0. Sum will be 0
+// If user will choose cancel - it means null. Null to Number = 0. Sum will be 0
 
 const sum = firstNumber + secondNumber;
 alert('Your result is ' + firstNumber + '+' + secondNumber + '='  + sum);
