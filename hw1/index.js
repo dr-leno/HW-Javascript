9fd04fd0e01477c94e57c8ff49aa62b6d92569bb
Olena Dovban <dovbanelena@gmail.com>
2022-08-24 01:21:20 +0300

    change alert output

--- hw1/index.js
+++ hw1/index.js
@@ -1,12 +1,9 @@
 'use strict'
 
-let firstNumber = prompt('Enter first number', '2');
-let secondNumber = prompt('Enter second number', '3');
+const firstNumber = Number(prompt('Enter first number', 2));
+const secondNumber = Number(prompt('Enter second number', 3));
 
 // If user will choose cancel - it means false. False to Number = 0. Sum will be 0
 
-firstNumber = Number(firstNumber);
-secondNumber = Number(secondNumber);
-
 const sum = firstNumber + secondNumber;
-alert('Your result is ' + sum);
+alert('Your result is ' + firstNumber + '+' + secondNumber + '='  + sum);
