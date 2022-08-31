b5010830e64c9986214920837776b712c7fce7e7
Olena Dovban <dovbanelena@gmail.com>
2022-09-01 01:49:11 +0300

    complete homework

---
+++ hw3/index.js
@@ -0,0 +1,31 @@
+'use strict'
+const number = getOperand('Enter number');
+
+function getOperand(label) {
+    let operand;
+
+    do {
+        operand = prompt(label);
+    } while (isOperandInvalid(operand));
+
+    return Number(operand);
+}
+function isOperandInvalid(val) {
+    return val === null || val <= 0 || val.trim() === '' || isNaN(val);
+}
+
+let sumEven = 0;
+let sumOdd = 0;
+
+// цикл, который будет выполняться начиная с 1 и увеличиваясь на 1 (посткремент) пока не будет равен введенному числу
+for (let i = 1; i <= number; i++) {
+    if (i % 2 == 0) {
+        sumEven +=i;
+    } else {
+        sumOdd +=i;
+ }
+}
+
+alert(`Your number ${number}
+Sum of even numbers ${sumEven}
+Sum of odd numbers ${sumOdd}`);
