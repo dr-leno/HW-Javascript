de52b8a2b79ac1dbe1b97e0793dd59271fbce6d1
Olena Dovban <dovbanelena@gmail.com>
2022-09-22 13:44:34 +0300

    complete task8

---
+++ hw8/index.js
@@ -0,0 +1,34 @@
+const resultDiv = document.querySelector('#result')
+const calculateBtn = document.querySelector('#calculate');
+
+function onBtnCalculate(){
+    let operandA = document.querySelector('#number1').valueAsNumber;
+    let operandB = document.querySelector('#number2').valueAsNumber;
+    let action = document.querySelector('#action').value;
+    const answer = calculateResult(operandA, operandB, action);
+    if (isNaN(operandA) || isNaN(operandB)) {
+        resultDiv.style.color = 'red';
+        resultDiv.textContent = 'ERROR Please, enter number';
+    }
+    else{
+        resultDiv.style.color = 'black';
+        resultDiv.textContent = `Expression: ${operandA} ${action} ${operandB} = ${answer}`; 
+    }
+}
+
+function calculateResult(a, b, action) {
+        switch (action) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                return a / b;
+        }
+}
+
+
+calculateBtn.addEventListener('click', onBtnCalculate);
+
