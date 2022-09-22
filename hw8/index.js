03aef81b064d41d6aed583e43910c14465c34f26
Olena Dovban <dovbanelena@gmail.com>
2022-09-22 14:06:20 +0300

    fix counting condition

--- hw8/index.js
+++ hw8/index.js
@@ -1,16 +1,16 @@
-const resultDiv = document.querySelector('#result')
+const resultDiv = document.querySelector('#result');
 const calculateBtn = document.querySelector('#calculate');
 
 function onBtnCalculate(){
     let operandA = document.querySelector('#number1').valueAsNumber;
     let operandB = document.querySelector('#number2').valueAsNumber;
     let action = document.querySelector('#action').value;
-    const answer = calculateResult(operandA, operandB, action);
     if (isNaN(operandA) || isNaN(operandB)) {
         resultDiv.style.color = 'red';
         resultDiv.textContent = 'ERROR Please, enter number';
     }
     else{
+        const answer = calculateResult(operandA, operandB, action);
         resultDiv.style.color = 'black';
         resultDiv.textContent = `Expression: ${operandA} ${action} ${operandB} = ${answer}`; 
     }
@@ -28,7 +28,6 @@ function calculateResult(a, b, action) {
                 return a / b;
         }
 }
-
 
 calculateBtn.addEventListener('click', onBtnCalculate);
 
