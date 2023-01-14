e57a2b56b3f644f5ec70c48ee4404f7452db1891
Olena Dovban <dovbanelena@gmail.com>
2023-01-14 20:01:45 +0200

    complete task27

---
+++ hw27-react-users-routing/src/modules/users/pages/UserForm.js
@@ -0,0 +1,82 @@
+import { Box, Button, Paper, TextField } from '@mui/material';
+import { NavLink, useNavigate, useParams } from 'react-router-dom';
+
+import React from 'react';
+import useUserForm from '../hooks/useUserForm';
+
+function UserForm() {
+  const { id } = useParams();
+  const { user, changeUser, saveUser } =
+    useUserForm(id);
+
+  const navigate = useNavigate();
+
+  function onInputChange(e) {
+    changeUser({ [e.target.name]: e.target.value });
+  }
+
+
+  function onFormSubmit(e) {
+    e.preventDefault();
+
+    saveUser(user).then(() => {
+      navigate('..');
+    });
+  }
+
+  return (
+    <Paper>
+      <Box
+        onSubmit={onFormSubmit}
+        component="form"
+        sx={{
+          '& .MuiTextField-root, button': {
+            m: 1,
+            maxWidth: '-webkit-fill-available',
+          },
+        }}
+      >
+        <TextField
+          name="name"
+          label="Name"
+          value={user.name}
+          onChange={onInputChange}
+          
+          fullWidth
+         
+        />
+        <TextField
+          name="surname"
+          label="Surname"
+          value={user.surname}
+          onChange={onInputChange}
+          
+          fullWidth
+          
+        />
+        <TextField
+          name="email"
+          label="Email"
+          value={user.email}
+          onChange={onInputChange}
+         
+          fullWidth
+          
+        />
+        <Button variant="text" color="error" to=".." component={NavLink}>
+          Cancel
+        </Button>
+        <Button
+          variant="contained"
+          type="submit"
+          color="primary"
+         
+        >
+          Save
+        </Button>
+      </Box>
+    </Paper>
+  );
+}
+
+export default UserForm;
